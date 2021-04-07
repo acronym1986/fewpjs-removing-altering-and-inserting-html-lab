7d793992f7b202dbceda9269756c68c0217459cc
Tim <acronym1986@gmail.com>
2021-04-07 23:57:49 +1000

    done

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+//document.removeChild(document.querySelector("main#main"))
+main.remove()
+
+const newHeader = document.createElement ('H1');
+
+      newHeader.id = "victory";
+
+     newHeader.innerHTML = 'YOUR-NAME is the champion';
